e9666182d6120f1a789d20c7d3a3115c05e1e8a8
naro <obokerov@v6services.ru>
2025-02-04 19:35:19 +0300

    fucking vaadin start to work in a right way

--- src/main/java/com/example/mssfrontend/model/employee/entity/Employee.java
+++ src/main/java/com/example/mssfrontend/model/employee/entity/Employee.java
@@ -5,11 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name = "employees")
+@NoArgsConstructor
 @Setter
 @Getter
 @ToString
